3aa0ad2f016efad0b766cba7bd7277f1c322cc2a
jwojcikow127 <80952694+jwojcikow127@users.noreply.github.com>
2023-06-17 23:19:24 +0200

    First commit- created a project, configured hardware - tft display

---
+++ Core/Inc/lcd.h
@@ -0,0 +1,19 @@
+/*
+ * lcd.h
+ *
+ *  Created on: Jun 17, 2023
+ *      Author: jwoj2
+ */
+
+#ifndef INC_LCD_H_
+#define INC_LCD_H_
+
+
+
+#endif /* INC_LCD_H_ */
+
+#pragma once
+
+#include <stdint.h>
+
+void lcd_init(void);
---
+++ Core/Src/lcd.c
@@ -0,0 +1,33 @@
+/*
+ * lcd.c
+ *
+ *  Created on: Jun 17, 2023
+ *      Author: jwoj2
+ */
+
+#include "lcd.h"
+#include "spi.h"
+
+//definition of tft display commands magic numbers
+#define ST7735S_SLPOUT			0x11
+#define ST7735S_DISPOFF			0x28
+#define ST7735S_DISPON			0x29
+#define ST7735S_CASET			0x2a
+#define ST7735S_RASET			0x2b
+#define ST7735S_RAMWR			0x2c
+#define ST7735S_MADCTL			0x36
+#define ST7735S_COLMOD			0x3a
+#define ST7735S_FRMCTR1			0xb1
+#define ST7735S_FRMCTR2			0xb2
+#define ST7735S_FRMCTR3			0xb3
+#define ST7735S_INVCTR			0xb4
+#define ST7735S_PWCTR1			0xc0
+#define ST7735S_PWCTR2			0xc1
+#define ST7735S_PWCTR3			0xc2
+#define ST7735S_PWCTR4			0xc3
+#define ST7735S_PWCTR5			0xc4
+#define ST7735S_VMCTR1			0xc5
+#define ST7735S_GAMCTRP1		0xe0
+#define ST7735S_GAMCTRN1		0xe1
+
+
